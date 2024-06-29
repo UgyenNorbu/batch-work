7f265a881bc8f2ad069c3b82e038e6a1285c235b
Ugyen Norbu <unorbu2019@gmail.com>
2024-06-29 10:58:11 +0600

    Modify for two functionalities

--- batch_work/main.py
+++ batch_work/main.py
@@ -1,36 +1,28 @@
 import os
 import sys
 
-def bw_rename(directory_path, prefix):
-    if not os.path.isdir(directory_path):
-        print(f"Error: {directory_path} is not a valid directory.")
-        return
-    
-    directory_name = os.path.basename(directory_path)
-    
-    files = []
-    for f in os.listdir(directory_path):
-        full_path = os.path.join(directory_path, f)
-        if os.path.isfile(full_path):
-            files.append(f)
-    
-    files.sort()
-    
-    for index, filename in enumerate(files, start=1):
-        file_extension = os.path.splitext(filename)[1]
-        new_filename = f"{prefix}_{index}{file_extension}"
-        os.rename(os.path.join(directory_path, filename), os.path.join(directory_path, new_filename))
-        print(f"Renamed: {filename} -> {new_filename}")
+from .bw_sort import bw_sort
+from .bw_rename import bw_rename
 
 def main():
-    if len(sys.argv) != 3:
-        print("Enter a directory path as an argument.")
+    if len(sys.argv) < 2:
+        print("Error: Please follow the following usage")
         print("Usage: bw_rename <prefix> <directory_path>")
-        sys.exit(1)
+        print("Usage: bw_sort <directory_path>")
 
-    prefix = sys.argv[1]
-    directory_path = sys.argv[2]
-    bw_rename(directory_path, prefix)
+    elif 'bw_sort' in sys.argv[0] and len(sys.argv) == 2:
+        directory_path = sys.argv[1]
+        bw_sort(directory_path)
+        
+    elif 'bw_rename' in sys.argv[0] and len(sys.argv) == 3:
+        prefix = sys.argv[1]
+        directory_path = sys.argv[2]
+        bw_rename(directory_path, prefix)
+
+    else:
+        print("Error: Please follow the following usage")
+        print("Usage: bw_rename <prefix> <directory_path>")
+        print("Usage: bw_sort <directory_path>")
 
 if __name__ == "__main__":
     main()
