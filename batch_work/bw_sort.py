e67d32cac30892748d5343b9b0a5e4f35a61be86
Ugyen Norbu <unorbu2019@gmail.com>
2024-06-30 10:08:14 +0600

    License note added in souce code

--- batch_work/bw_sort.py
+++ batch_work/bw_sort.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024 Ugyen Norbu
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import os
 import sys
 import subprocess
