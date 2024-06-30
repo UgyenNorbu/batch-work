f6b14bdac6e030542df397ae812ccf2b8d4de809
Ugyen Norbu <unorbu2019@gmail.com>
2024-06-30 10:39:24 +0600

    Change v0.1 to v0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="batch_work",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     entry_points={
         'console_scripts':[
