29bb7054bf6bbabe359db5bd027c764da2e8afc4
Ugyen Norbu <unorbu2019@gmail.com>
2024-07-09 17:55:58 +0600

    Change version no in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="batch-work",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     license="MIT",
     author_email='unorbu2007@gmail.com',
